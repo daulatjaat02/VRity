dd255bfa239d14b50bf616a5aedc75ee3542329c
Daulat Jajra <daulatjajra2006@gmail.com>
2023-12-27 20:58:40 +0530

    'sections'

---
+++ script.js
@@ -0,0 +1,11 @@
+// ///// sidebar
+// let sidebar = document.querySelector(".sidebar");
+// let closeBtn = document.getElementById("close-btn");
+// let navContainer = document.querySelector(".navContainer");
+
+// sidebar.addEventListener("click", () => {
+//   navContainer.classList.toggle("show-sidebar");
+// });
+// closeBtn.addEventListener("click", function () {
+//   navContainer.classList.remove("show-sidebar");
+// });
